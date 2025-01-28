0e331a92e6509ec61db298d7797e7e86afbd986f
BJill1 <157993588+BJill1@users.noreply.github.com>
2025-01-28 03:04:48 +0000

    Beginning code of Devops demo

---
+++ devops_demo/factorial_library/FactorialService.cs
@@ -0,0 +1,14 @@
+﻿namespace factorial_library;
+
+public class FactorialService
+{
+    public static int factorial_function(int n)
+    {
+        if(n == 0 || n == 1){
+            return 1;
+        } else{
+            return n * factorial_function(n-1);
+        }
+    }
+
+}
---
+++ factorial_library.Tests/UnitTest1.cs
@@ -0,0 +1,10 @@
+namespace factorial_library.Tests;
+
+public class UnitTest1
+{
+    [Fact]
+    public void Test1()
+    {
+
+    }
+}
